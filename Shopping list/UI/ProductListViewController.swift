0d1afd867aa350968a444cb7c34e2f8705e85bc9
Alina.Petrovskaya <alina.petrovskaia92@gmail.com>
2020-12-02 21:22:46 +0200

    refactor extensions of main controller

--- Shopping list/UI/ProductListViewController.swift
+++ Shopping list/UI/ProductListViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
-class ProductListViewController: UIViewController {
+class ProductListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.estimatedRowHeight = 100
         tableView.delegate   = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "ProductCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
         loadItems()
-        tableView.estimatedRowHeight = 100
+        
     }
 
     @IBAction func sortButtonTapped(_ sender: UIBarButtonItem) {
@@ -47,11 +48,8 @@ class ProductListViewController: UIViewController {
         }
     }
     
-    private func reloadData() {
-        tableView.reloadData()
-    }
     
-    func saveData() {
+    private func saveData() {
         guard let dataFile = dataFilePath else { return }
         let encoder = PropertyListEncoder()
         do {
@@ -63,7 +61,7 @@ class ProductListViewController: UIViewController {
         }
     }
     
-    func loadItems() {
+    private func loadItems() {
         guard let dataFile = dataFilePath else { return }
         if let data = try? Data(contentsOf: dataFile) {
             let decoder = PropertyListDecoder()
@@ -73,6 +71,126 @@ class ProductListViewController: UIViewController {
                 print("Error at reading data: \(error)")
             }
         }
+    }
+    
+    private func reloadData() {
+        tableView.reloadData()
+    }
+    
+    //MARK: - : UITableViewDataSource
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataShoppingList.arrayOfProducts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! ProductCell
+        cell.name.text = DataShoppingList.arrayOfProducts[indexPath.row].name
+        cell.descriptionLabel.text = DataShoppingList.arrayOfProducts[indexPath.row].description
+        cell.raiting = DataShoppingList.arrayOfProducts[indexPath.row].raiting
+        return cell
+    }
+    
+    //MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let delete = deleteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let edit = editAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [edit])
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        DataShoppingList.arrayOfProducts.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        saveData()
+    }
+    
+    private func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { (_,_,_) in
+            DataShoppingList.arrayOfProducts.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            self.saveData()
+        }
+        action.image = UIImage.init(systemName: "trash")
+        action.backgroundColor = .red
+        
+        return action
+    }
+    
+    private func editAction(at indexPath: IndexPath) -> UIContextualAction {
+        
+        let action = UIContextualAction(style: .normal, title: "Edit") { (_,_,success) in
+            let array = DataShoppingList.arrayOfProducts[indexPath.row]
+            self.alertShow(currentName: array.name, currentDiscription: array.description, currentRaiting: array.raiting, numberOFcell: indexPath)
+            self.saveData()
+            success(true)
+        }
+        
+        action.image = UIImage.init(systemName: "square.and.pencil")
+        action.backgroundColor = .orange
+        return action
+    }
+    
+    //MARK: - UITextFieldDelegate
+    
+    
+    //MARK: - Alert
+    private func alertShow(currentName: String, currentDiscription: String, currentRaiting: Int, numberOFcell: IndexPath) {
+        
+        var nameField = UITextField() //хранит название нового списка
+        var descriptionField = UITextField()
+        var raitingField = UITextField()
+        
+        let alert = UIAlertController(title: "Edit Item", message: "", preferredStyle: .alert)
+        
+        let editAction = UIAlertAction(title: "Save changes", style: .destructive) { (action) in
+            guard let nametext = nameField.text, let descriptionText = descriptionField.text else { return }
+            if let raiting = raitingField.text {
+                let name = nametext == "" ? "No name" : nametext
+                DataShoppingList.arrayOfProducts[numberOFcell.row].name = name
+                DataShoppingList.arrayOfProducts[numberOFcell.row].description = descriptionText
+                DataShoppingList.arrayOfProducts[numberOFcell.row].raiting = Int(raiting) ?? currentRaiting
+            }
+            self.saveData()
+            self.tableView.reloadRows(at: [numberOFcell], with: .automatic)
+        }
+        
+        let dismissAction = UIAlertAction(title: "Close", style: .cancel) { (action) in
+                self.dismiss(animated: true, completion: nil)
+            }
+        
+        alert.addTextField { (name) in // добавляем поле для ввода текста
+            name.text = currentName
+            name.placeholder = "Enter name"
+            nameField = name
+        }
+        
+        alert.addTextField { (description) in // добавляем поле для ввода текста
+            description.text = currentDiscription
+            description.placeholder = "Enter description of product"
+            descriptionField = description
+        }
+        
+        alert.addTextField { (raiting) in // добавляем поле для ввода текста
+            raiting.text = "\(currentRaiting)"
+            raiting.placeholder = "Value from 0 to 5"
+            raiting.keyboardType = .numberPad
+            raitingField = raiting
+        }
+        
+        alert.addAction(editAction) //вызываем действие и презентуем алерт
+        alert.addAction(dismissAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
