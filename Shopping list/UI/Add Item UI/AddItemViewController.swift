217ba63c098a3ca2e912d52e23a21e1058d62741
Alina.Petrovskaya <alina.petrovskaia92@gmail.com>
2020-12-03 20:56:32 +0200

    code refactoring

---
+++ Shopping list/UI/Add Item UI/AddItemViewController.swift
@@ -0,0 +1,74 @@
+//
+//  AddItemViewController.swift
+//  Shopping list
+//
+//  Created by Виктор Петровский on 29.11.2020.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    public var callback: ((String, String, Int) -> ())?
+    private var raitingValues = [1, 2, 3, 4, 5]
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var raitingPicker: UIPickerView!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        raitingPicker.dataSource = self
+        raitingPicker.delegate = self
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector (self.tapContentView (_:)))
+        let tapMain = UITapGestureRecognizer(target: self, action: #selector (self.tapMainView (_:)))
+        self.contentView.addGestureRecognizer(tap)
+        self.mainView.addGestureRecognizer(tapMain)
+        updateUI()
+    }
+    
+    @objc func tapContentView(_ sender:UITapGestureRecognizer) {
+        nameTextField.endEditing(true)
+        descriptionTextField.endEditing(true)
+    }
+    
+    @objc func tapMainView(_ sender:UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveDataPressed(_ sender: UIButton) {
+        let raitingValue = raitingValues[raitingPicker.selectedRow(inComponent: 0)]
+        
+        guard let nameText = nameTextField.text, let descriptionText = descriptionTextField.text else { return }
+        
+        let name = nameText == "" ? "No value" : nameText
+        callback?(name, descriptionText, raitingValue)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    private func updateUI() {
+        saveButton.layer.cornerRadius = saveButton.frame.height / 2
+    }
+    
+    //MARK: - UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return raitingValues.count
+    }
+    
+    //MARK: - UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(raitingValues[row])"
+    }
+    
+}
